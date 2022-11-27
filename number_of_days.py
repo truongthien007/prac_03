11c5c8a204cba3255fc1913a62fa2762ee27650a
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-11-27 15:17:41 +0700

    add file

--- number_of_days.py
+++ number_of_days.py
@@ -7,13 +7,17 @@ a 50% chance that it decreases by 0 to 5%.
 If the price rises above $100, or falls below $1, the program should end.
 The price should be displayed to the nearest cent (e.g. $33.59, not $33.5918232901)
 """
+
 import random
 
+OUTPUT_FILE = "output_file.txt"
 MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1
 MAX_PRICE = 100
 INITIAL_PRICE = 10.0
+
+out_file = open(OUTPUT_FILE, 'w')
 day = 0
 price = INITIAL_PRICE
 print(f"${price:,.2f}")
@@ -34,3 +38,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
     price *= (1 + price_change)
     day += 1
     print(f"On day {day} price is: ${price:,.2f}")
+    print(f"On day {day} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
