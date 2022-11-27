186f94eb4933b726d0e88672ea802b22110d9b55
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-11-27 14:49:50 +0700

    complete task

---
+++ randoms.py
@@ -0,0 +1,18 @@
+"""I saw a random number
+the smallest one is 6 and the biggest I can see is 20
+"""
+
+"""
+I saw a random number
+the smallest one is 3, the biggest is 9
+it can  not print 4
+"""
+
+"""
+the smallest I can see is 2.995355859626244
+the biggest I can see is 5.493798015575292
+"""
+
+import random
+
+print(random.randint(0, 101))
