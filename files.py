a794e3f9d0c97201c71725fa95c9a93578dff0e3
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-11-27 16:52:35 +0700

    complete task

--- files.py
+++ files.py
@@ -1,18 +1,18 @@
-# # 1. Write code that asks the user for their name, then opens a file called "name.txt" and writes that name to it
+# 1. Write code that asks the user for their name, then opens a file called "name.txt" and writes that name to it
 NAME = "name.txt"
 out_file = open(NAME, 'w')
 name = input("Enter your name: ")
 print(name, file=out_file)
 out_file.close()
 
-# # 2. Write code that opens "name.txt" and reads the name (as above) then prints, "Your name is Bob"
+# 2. Write code that opens "name.txt" and reads the name (as above) then prints, "Your name is Bob"
 NAME = "name.txt"
 in_file = open(NAME)
 text = in_file.read()
 in_file.close()
 print(f"your name is {text}")
 
-# # 3. Write code that opens "numbers.txt", reads only the first two numbers and adds them together then prints the result
+# 3. Write code that opens "numbers.txt", reads only the first two numbers and adds them together then prints the  result
 FILENAME = "numbers.txt"
 in_file = open(FILENAME)
 numbers = in_file.readlines()
