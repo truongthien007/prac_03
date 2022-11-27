f0b49a146821d3cab47f59df6d4e3d6ccaea6e38
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-11-27 17:01:42 +0700

    put the count day inside

--- capitalist_conrad.py
+++ capitalist_conrad.py
@@ -2,19 +2,23 @@
 CP1404/CP5632 - Practical
 Capitalist Conrad wants a stock price simulator for a volatile stock.
 The price starts off at $10.00, and, at the end of every day there is
-a 50% chance it increases by 0 to 10%, and
+a 50% chance it increases by 0 to 17.5%, and
 a 50% chance that it decreases by 0 to 5%.
-If the price rises above $1000, or falls below $0.01, the program should end.
+If the price rises above $100, or falls below $1, the program should end
 The price should be displayed to the nearest cent (e.g. $33.59, not $33.5918232901)
 """
+
 import random
 
-MAX_INCREASE = 0.1  # 10%
+OUTPUT_FILE = "output_file.txt"
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1
+MAX_PRICE = 100
 INITIAL_PRICE = 10.0
 
+out_file = open(OUTPUT_FILE, 'w')
+day = 0
 price = INITIAL_PRICE
 print(f"${price:,.2f}")
 
@@ -32,4 +36,8 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    day += 1
+    print(f"On day {day} price is: ${price:,.2f}")
+    print(f"On day {day} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
