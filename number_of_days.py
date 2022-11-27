65d39962af2fc12d30a95f22092c0086a764d355
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-11-27 15:08:04 +0700

    change the constants value

--- number_of_days.py
+++ number_of_days.py
@@ -2,17 +2,17 @@
 CP1404/CP5632 - Practical
 Capitalist Conrad wants a stock price simulator for a volatile stock.
 The price starts off at $10.00, and, at the end of every day there is
-a 50% chance it increases by 0 to 10%, and
+a 50% chance it increases by 0 to 17.5%, and
 a 50% chance that it decreases by 0 to 5%.
-If the price rises above $1000, or falls below $0.01, the program should end.
+If the price rises above $100, or falls below $1, the program should end.
 The price should be displayed to the nearest cent (e.g. $33.59, not $33.5918232901)
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1
+MAX_PRICE = 100
 INITIAL_PRICE = 10.0
 day = 0
 price = INITIAL_PRICE
